67121c2201d91b08540fa282f7ca7e5a47460738
ahmedamrwagieh <ahmedamrwagieh@gmail.com>
2026-07-05 17:33:32 +0300

    Delete home.js

--- home.js
+++
@@ -1,7 +0,0 @@
-const loginBtn = document.getElementById("loginBtn");
-const signupBtn = document.getElementById("signupBtn");
-
-if (sessionStorage.getItem("isLoggedIn") === "true") {
-    loginBtn.style.display = "none";
-    signupBtn.style.display = "none";
-}
